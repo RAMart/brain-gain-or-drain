dee95180c2c905e242748b2ec6cad9021597bb9e
Ingo Küper <ingo_kueper@hotmail.com>
2015-08-15 23:37:23 +0200

    Version update: clojure and neko

--- android/project.clj
+++ android/project.clj
@@ -1,12 +1,12 @@
 (defproject brain-gain-or-drain "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
-  
+
   :dependencies [[com.badlogicgames.gdx/gdx "1.5.5" :use-resources true]
                  [com.badlogicgames.gdx/gdx-backend-android "1.5.5"]
                  [com.badlogicgames.gdx/gdx-box2d "1.5.5"]
                  [com.badlogicgames.gdx/gdx-bullet "1.5.5"]
-                 [neko/neko "3.2.0-preview3"]
-                 [org.clojure-android/clojure "1.7.0-alpha5" :use-resources true]
+                 [neko/neko "3.2.0"]
+                 [org.clojure-android/clojure "1.7.0" :use-resources true]
                  [play-clj "0.4.5"]]
   :profiles {:dev {:dependencies [[org.clojure-android/tools.nrepl "0.2.6-lollipop"]]
                    :android {:aot :all-with-unused}}
@@ -17,14 +17,14 @@
                         ;; :keystore-path "/home/user/.android/private.keystore"
                         ;; :key-alias "mykeyalias"
                         :aot :all}}}
-  
+
   :android {;; Specify the path to the Android SDK directory either
             ;; here or in your ~/.lein/profiles.clj file.
             ;; :sdk-path "/home/user/path/to/android-sdk/"
-            
+
             ;; Uncomment this if dexer fails with OutOfMemoryException
             ;; :force-dex-optimize true
-            
+
             :assets-paths ["../desktop/resources"]
             :native-libraries-paths ["libs"]
             :target-version "15"
@@ -33,7 +33,7 @@
                              "cljs-tooling.util.analysis" "cljs-tooling.util.misc"
                              "cider.nrepl" "cider-nrepl.plugin"]
             :dex-opts ["-JXmx4096M"]}
-  
+
   :source-paths ["src/clojure" "../desktop/src-common"]
   :java-source-paths ["src/java"]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"])
